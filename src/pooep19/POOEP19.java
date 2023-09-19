419ad7970d8ae28e12b3770c3d34886c3db87fe3
JuanEstebanGMA <jugomezac@unal.edu.co>
2023-09-18 21:34:47 -0500

    POOEP19

---
+++ src/pooep19/POOEP19.java
@@ -0,0 +1,10 @@
+
+package pooep19;
+
+public class POOEP19 {
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+    
+}
